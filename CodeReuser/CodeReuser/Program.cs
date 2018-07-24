e43a1af183ebf2c4d781524e10e28aaa7d08bd1b
Snir Haphiloni <snhaphil@microsoft.com>
2018-07-24 10:54:38 +0300

    added the search code

---
+++ CodeReuser/CodeReuser/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CodeReuser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunQueryAsync("Va").Wait();
+        }
+
+        private static async Task RunQueryAsync(string text)
+        {
+            try
+            {
+                VisualStudioCodeSearchHelper vsoSearch = new VisualStudioCodeSearchHelper();
+                var searchResults = await vsoSearch.RunSearchQueryAsync(
+                    new CodeSearchQuery
+                    {
+                        SearchText = text,
+                        QuerySearchFilters = new CodeSearchFilters
+                        {
+                            Project = new string[] { "One" },
+                        },
+                        SkipResults = 0,
+                        TakeResults = 100
+                    }).ConfigureAwait(false);
+                Console.WriteLine(searchResults.Count);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ CodeReuser/CodeReuser/VisualStudioHttpClientPool.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.WebApi;
+
+namespace CodeReuser
+{
+    public class VisualStudioHttpClientPool
+    {
+        private static ConcurrentDictionary<string, HttpClient> basicHttpClientCache = new ConcurrentDictionary<string, HttpClient>();
+        private static ConcurrentDictionary<string, VssHttpClientBase> vssHttpClientCache = new ConcurrentDictionary<string, VssHttpClientBase>();
+
+        private static ConcurrentDictionary<string, VssConnection> vssConnectionCache = new ConcurrentDictionary<string, VssConnection>();
+
+
+        public static VssConnection GetConnection(Uri uri)
+        {
+            var connection = new VssConnection(uri, new VssClientCredentials());
+            return connection;
+        }
+
+        public static HttpClient GetBasicHttpClient(Uri uri)
+        {
+            string cacheKey = uri.OriginalString;
+
+            if (!basicHttpClientCache.ContainsKey(cacheKey))
+            {
+                var httpClient = new HttpClient();
+
+                httpClient.BaseAddress = uri;
+                httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "");
+
+                basicHttpClientCache.TryAdd(uri.OriginalString, httpClient);
+            }
+
+            return basicHttpClientCache[uri.OriginalString];
+        }
+
+        public static T GetVssHttpClient<T>(Uri uri) where T : VssHttpClientBase
+        {
+            string cacheKey = uri.OriginalString;
+
+            if (!vssHttpClientCache.ContainsKey(cacheKey))
+            {
+                VssConnection vssConnection = GetConnection(uri);
+
+                //var vssConnection = new VssConnection(uri, new VssAadCredential());
+                //var clientCredentials = new VssClientCredentials(new VssBasicCredential("<UserName>","<Password-FromKeyVault>"));
+                //vssConnection = new VssConnection(this.TeamUri, clientCredentials);
+
+                vssHttpClientCache.TryAdd(uri.OriginalString, vssConnection.GetClient<T>());
+            }
+
+            return vssHttpClientCache[uri.OriginalString] as T;
+        }
+    }
+}
